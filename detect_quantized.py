b06c7e61d7fde714b233fe1dd13444d381cd07c1
Sacha Refshauge <srefshauge@imagus.com.au>
2020-10-12 08:16:24 +1000

    Add mkldnn option to torchscript generator

--- detect_quantized.py
+++ detect_quantized.py
@@ -13,12 +13,14 @@ from utils.box_utils import decode, decode_landm
 import time
 import torch.quantization
 from torch.utils.mobile_optimizer import optimize_for_mobile
+from torch.utils import mkldnn
 
 parser = argparse.ArgumentParser(description='Retinaface')
 
 parser.add_argument('-m', '--trained_model', default='./weights/Final_Retinaface.pth',
                     type=str, help='Trained state_dict file path to open')
 parser.add_argument('--cpu', action="store_true", default=False, help='Use cpu inference')
+parser.add_argument('--mkl', action="store_true", default=False, help='Use mkldnn inference')
 parser.add_argument('--confidence_threshold', default=0.05, type=float, help='confidence_threshold')
 parser.add_argument('--top_k', default=5000, type=int, help='top_k')
 parser.add_argument('--nms_threshold', default=0.3, type=float, help='nms_threshold')
@@ -79,6 +81,8 @@ def test(model, device, args):
         img = img.transpose(2, 0, 1)
         img = torch.from_numpy(img).unsqueeze(0)
         img = img.to(device)
+        if args.mkl:
+            img = img.to_mkldnn()
         scale = scale.to(device)
 
         tic = time.time()
@@ -167,53 +171,113 @@ if __name__ == '__main__':
     #net.fuse_model()
     # set quantization config for server (x86)
     net.qconfig = torch.quantization.get_default_qconfig('fbgemm')
-    torch.quantization.fuse_modules(net, [['body.stage1.0.0', 'body.stage1.0.1', 'body.stage1.0.2'],
-                                          ['body.stage1.1.0', 'body.stage1.1.1', 'body.stage1.1.2'],
-                                          ['body.stage1.1.3', 'body.stage1.1.4', 'body.stage1.1.5'],
-                                          ['body.stage1.2.0', 'body.stage1.2.1', 'body.stage1.2.2'],
-                                          ['body.stage1.2.3', 'body.stage1.2.4', 'body.stage1.2.5'],
-                                          ['body.stage1.3.0', 'body.stage1.3.1', 'body.stage1.3.2'],
-                                          ['body.stage1.3.3', 'body.stage1.3.4', 'body.stage1.3.5'],
-                                          ['body.stage1.4.0', 'body.stage1.4.1', 'body.stage1.4.2'],
-                                          ['body.stage1.4.3', 'body.stage1.4.4', 'body.stage1.4.5'],
-                                          ['body.stage1.5.0', 'body.stage1.5.1', 'body.stage1.5.2'],
-                                          ['body.stage1.5.3', 'body.stage1.5.4', 'body.stage1.5.5'],
-                                          ['body.stage2.0.0', 'body.stage2.0.1', 'body.stage2.0.2'],
-                                          ['body.stage2.0.3', 'body.stage2.0.4', 'body.stage2.0.5'],
-                                          ['body.stage2.1.0', 'body.stage2.1.1', 'body.stage2.1.2'],
-                                          ['body.stage2.1.3', 'body.stage2.1.4', 'body.stage2.1.5'],
-                                          ['body.stage2.2.0', 'body.stage2.2.1', 'body.stage2.2.2'],
-                                          ['body.stage2.2.3', 'body.stage2.2.4', 'body.stage2.2.5'],
-                                          ['body.stage2.3.0', 'body.stage2.3.1', 'body.stage2.3.2'],
-                                          ['body.stage2.3.3', 'body.stage2.3.4', 'body.stage2.3.5'],
-                                          ['body.stage2.4.0', 'body.stage2.4.1', 'body.stage2.4.2'],
-                                          ['body.stage2.4.3', 'body.stage2.4.4', 'body.stage2.4.5'],
-                                          ['body.stage2.5.0', 'body.stage2.5.1', 'body.stage2.5.2'],
-                                          ['body.stage2.5.3', 'body.stage2.5.4', 'body.stage2.5.5'],
-                                          ['body.stage3.0.0', 'body.stage3.0.1', 'body.stage3.0.2'],
-                                          ['body.stage3.0.3', 'body.stage3.0.4', 'body.stage3.0.5'],
-                                          ['body.stage3.1.0', 'body.stage3.1.1', 'body.stage3.1.2'],
-                                          ['body.stage3.1.3', 'body.stage3.1.4', 'body.stage3.1.5'],
-                                          ['fpn.output1.0', 'fpn.output1.1', 'fpn.output1.2'],
-                                          ['fpn.output2.0', 'fpn.output2.1', 'fpn.output2.2'],
-                                          ['fpn.output3.0', 'fpn.output3.1', 'fpn.output3.2'],
-                                          ['fpn.merge1.0', 'fpn.merge1.1', 'fpn.merge1.2'],
-                                          ['fpn.merge2.0', 'fpn.merge2.1', 'fpn.merge2.2'],
-                                          ['ssh1.conv3X3.0',   'ssh1.conv3X3.1'],
-                                          ['ssh1.conv5X5_1.0', 'ssh1.conv5X5_1.1', 'ssh1.conv5X5_1.2'],
-                                          ['ssh1.conv5X5_2.0', 'ssh1.conv5X5_2.1'],
-                                          ['ssh1.conv7X7_2.0', 'ssh1.conv7X7_2.1', 'ssh1.conv7X7_2.2'],
-                                          ['ssh1.conv7x7_3.0', 'ssh1.conv7x7_3.1'],
-                                          ['ssh2.conv3X3.0',   'ssh2.conv3X3.1'],
-                                          ['ssh2.conv5X5_1.0', 'ssh2.conv5X5_1.1', 'ssh2.conv5X5_1.2'],
-                                          ['ssh2.conv5X5_2.0', 'ssh2.conv5X5_2.1'],
-                                          ['ssh2.conv7X7_2.0', 'ssh2.conv7X7_2.1', 'ssh2.conv7X7_2.2'],
-                                          ['ssh2.conv7x7_3.0', 'ssh2.conv7x7_3.1'],
-                                          ['ssh3.conv3X3.0',   'ssh3.conv3X3.1'],
-                                          ['ssh3.conv5X5_1.0', 'ssh3.conv5X5_1.1', 'ssh3.conv5X5_1.2'],
-                                          ['ssh3.conv5X5_2.0', 'ssh3.conv5X5_2.1'],
-                                          ['ssh3.conv7X7_2.0', 'ssh3.conv7X7_2.1', 'ssh3.conv7X7_2.2'],
-                                          ['ssh3.conv7x7_3.0', 'ssh3.conv7x7_3.1'],
+    torch.quantization.fuse_modules(net, [
+        ['body.0.conv.0.interstellar1a_shufflenet_5x5_branch2a',   'body.0.conv.0.bn1a_shufflenet_5x5_branch2a', 'body.0.conv.0.relu1a_shufflenet_5x5_branch2a'],
+        ['body.0.conv.1.interstellar1a_shufflenet_5x5_branch2b_s', 'body.0.conv.1.interstellar1a_shufflenet_5x5_branch2b_s_bn'],
+        ['body.0.conv.1.interstellar1a_shufflenet_5x5_branch2b',   'body.0.conv.1.bn1a_shufflenet_5x5_branch2b'],
+        ['body.0.proj_conv.interstellar1a_proj_s',                 'body.0.proj_conv.interstellar1a_proj_s_bn'],
+        ['body.0.proj_conv.interstellar1a_proj',                   'body.0.proj_conv.bn1a_proj', 'body.0.proj_conv.relu1a_proj'],
+        ['body.1.conv.0.interstellar1b_xception_3x3_branch2a_s',   'body.1.conv.0.interstellar1b_xception_3x3_branch2a_s_bn'],
+        ['body.1.conv.0.interstellar1b_xception_3x3_branch2a',     'body.1.conv.0.bn1b_xception_3x3_branch2a', 'body.1.conv.0.relu1b_xception_3x3_branch2a'],
+        ['body.1.conv.1.interstellar1b_xception_3x3_branch2b_s',   'body.1.conv.1.interstellar1b_xception_3x3_branch2b_s_bn'],
+        ['body.1.conv.1.interstellar1b_xception_3x3_branch2b',     'body.1.conv.1.bn1b_xception_3x3_branch2b', 'body.1.conv.1.relu1b_xception_3x3_branch2b'],
+        ['body.1.conv.2.interstellar1b_xception_3x3_branch2c_s',   'body.1.conv.2.interstellar1b_xception_3x3_branch2c_s_bn'],
+        ['body.1.conv.2.interstellar1b_xception_3x3_branch2c',     'body.1.conv.2.bn1b_xception_3x3_branch2c'],
+        ['body.2.conv.0.interstellar1c_shufflenet_3x3_branch2a',   'body.2.conv.0.bn1c_shufflenet_3x3_branch2a', 'body.2.conv.0.relu1c_shufflenet_3x3_branch2a'],
+        ['body.2.conv.1.interstellar1c_shufflenet_3x3_branch2b_s', 'body.2.conv.1.interstellar1c_shufflenet_3x3_branch2b_s_bn'],
+        ['body.2.conv.1.interstellar1c_shufflenet_3x3_branch2b',   'body.2.conv.1.bn1c_shufflenet_3x3_branch2b'],
+        ['body.3.conv.0.interstellar1d_shufflenet_3x3_branch2a',   'body.3.conv.0.bn1d_shufflenet_3x3_branch2a', 'body.3.conv.0.relu1d_shufflenet_3x3_branch2a'],
+        ['body.3.conv.1.interstellar1d_shufflenet_3x3_branch2b_s', 'body.3.conv.1.interstellar1d_shufflenet_3x3_branch2b_s_bn'],
+        ['body.3.conv.1.interstellar1d_shufflenet_3x3_branch2b',   'body.3.conv.1.bn1d_shufflenet_3x3_branch2b'],
+        ['body.4.conv.0.interstellar2a_shufflenet_7x7_branch2a',   'body.4.conv.0.bn2a_shufflenet_7x7_branch2a', 'body.4.conv.0.relu2a_shufflenet_7x7_branch2a'],
+        ['body.4.conv.1.interstellar2a_shufflenet_7x7_branch2b_s', 'body.4.conv.1.interstellar2a_shufflenet_7x7_branch2b_s_bn'],
+        ['body.4.conv.1.interstellar2a_shufflenet_7x7_branch2b',   'body.4.conv.1.bn2a_shufflenet_7x7_branch2b'],
+        ['body.4.proj_conv.interstellar2a_proj_s',                 'body.4.proj_conv.interstellar2a_proj_s_bn'],
+        ['body.4.proj_conv.interstellar2a_proj',                   'body.4.proj_conv.bn2a_proj', 'body.4.proj_conv.relu2a_proj'],
+        ['body.5.conv.0.interstellar2b_xception_3x3_branch2a_s',   'body.5.conv.0.interstellar2b_xception_3x3_branch2a_s_bn'],
+        ['body.5.conv.0.interstellar2b_xception_3x3_branch2a',     'body.5.conv.0.bn2b_xception_3x3_branch2a', 'body.5.conv.0.relu2b_xception_3x3_branch2a'],
+        ['body.5.conv.1.interstellar2b_xception_3x3_branch2b_s',   'body.5.conv.1.interstellar2b_xception_3x3_branch2b_s_bn'],
+        ['body.5.conv.1.interstellar2b_xception_3x3_branch2b',     'body.5.conv.1.bn2b_xception_3x3_branch2b', 'body.5.conv.1.relu2b_xception_3x3_branch2b'],
+        ['body.5.conv.2.interstellar2b_xception_3x3_branch2c_s',   'body.5.conv.2.interstellar2b_xception_3x3_branch2c_s_bn'],
+        ['body.5.conv.2.interstellar2b_xception_3x3_branch2c',     'body.5.conv.2.bn2b_xception_3x3_branch2c'],
+        ['body.6.conv.0.interstellar2c_xception_3x3_branch2a_s',   'body.6.conv.0.interstellar2c_xception_3x3_branch2a_s_bn'],
+        ['body.6.conv.0.interstellar2c_xception_3x3_branch2a',     'body.6.conv.0.bn2c_xception_3x3_branch2a', 'body.6.conv.0.relu2c_xception_3x3_branch2a'],
+        ['body.6.conv.1.interstellar2c_xception_3x3_branch2b_s',   'body.6.conv.1.interstellar2c_xception_3x3_branch2b_s_bn'],
+        ['body.6.conv.1.interstellar2c_xception_3x3_branch2b',     'body.6.conv.1.bn2c_xception_3x3_branch2b', 'body.6.conv.1.relu2c_xception_3x3_branch2b'],
+        ['body.6.conv.2.interstellar2c_xception_3x3_branch2c_s',   'body.6.conv.2.interstellar2c_xception_3x3_branch2c_s_bn'],
+        ['body.6.conv.2.interstellar2c_xception_3x3_branch2c',     'body.6.conv.2.bn2c_xception_3x3_branch2c'],
+        ['body.7.conv.0.interstellar2d_xception_3x3_branch2a_s',   'body.7.conv.0.interstellar2d_xception_3x3_branch2a_s_bn'],
+        ['body.7.conv.0.interstellar2d_xception_3x3_branch2a',     'body.7.conv.0.bn2d_xception_3x3_branch2a', 'body.7.conv.0.relu2d_xception_3x3_branch2a'],
+        ['body.7.conv.1.interstellar2d_xception_3x3_branch2b_s',   'body.7.conv.1.interstellar2d_xception_3x3_branch2b_s_bn'],
+        ['body.7.conv.1.interstellar2d_xception_3x3_branch2b',     'body.7.conv.1.bn2d_xception_3x3_branch2b', 'body.7.conv.1.relu2d_xception_3x3_branch2b'],
+        ['body.7.conv.2.interstellar2d_xception_3x3_branch2c_s',   'body.7.conv.2.interstellar2d_xception_3x3_branch2c_s_bn'],
+        ['body.7.conv.2.interstellar2d_xception_3x3_branch2c',     'body.7.conv.2.bn2d_xception_3x3_branch2c'],
+        ['body.8.conv.0.interstellar3a_shufflenet_7x7_branch2a',   'body.8.conv.0.bn3a_shufflenet_7x7_branch2a', 'body.8.conv.0.relu3a_shufflenet_7x7_branch2a'],
+        ['body.8.conv.1.interstellar3a_shufflenet_7x7_branch2b_s', 'body.8.conv.1.interstellar3a_shufflenet_7x7_branch2b_s_bn'],
+        ['body.8.conv.1.interstellar3a_shufflenet_7x7_branch2b',   'body.8.conv.1.bn3a_shufflenet_7x7_branch2b'],
+        ['body.8.proj_conv.interstellar3a_proj_s',                 'body.8.proj_conv.interstellar3a_proj_s_bn'],
+        ['body.8.proj_conv.interstellar3a_proj',                   'body.8.proj_conv.bn3a_proj', 'body.8.proj_conv.relu3a_proj'],
+        ['body.9.conv.0.interstellar3b_xception_3x3_branch2a_s',   'body.9.conv.0.interstellar3b_xception_3x3_branch2a_s_bn'],
+        ['body.9.conv.0.interstellar3b_xception_3x3_branch2a',     'body.9.conv.0.bn3b_xception_3x3_branch2a', 'body.9.conv.0.relu3b_xception_3x3_branch2a'],
+        ['body.9.conv.1.interstellar3b_xception_3x3_branch2b_s',   'body.9.conv.1.interstellar3b_xception_3x3_branch2b_s_bn'],
+        ['body.9.conv.1.interstellar3b_xception_3x3_branch2b',     'body.9.conv.1.bn3b_xception_3x3_branch2b', 'body.9.conv.1.relu3b_xception_3x3_branch2b'],
+        ['body.9.conv.2.interstellar3b_xception_3x3_branch2c_s',   'body.9.conv.2.interstellar3b_xception_3x3_branch2c_s_bn'],
+        ['body.9.conv.2.interstellar3b_xception_3x3_branch2c',     'body.9.conv.2.bn3b_xception_3x3_branch2c'],
+        ['body.10.conv.0.interstellar3c_xception_3x3_branch2a_s',   'body.10.conv.0.interstellar3c_xception_3x3_branch2a_s_bn'],
+        ['body.10.conv.0.interstellar3c_xception_3x3_branch2a',     'body.10.conv.0.bn3c_xception_3x3_branch2a', 'body.10.conv.0.relu3c_xception_3x3_branch2a'],
+        ['body.10.conv.1.interstellar3c_xception_3x3_branch2b_s',   'body.10.conv.1.interstellar3c_xception_3x3_branch2b_s_bn'],
+        ['body.10.conv.1.interstellar3c_xception_3x3_branch2b',     'body.10.conv.1.bn3c_xception_3x3_branch2b', 'body.10.conv.1.relu3c_xception_3x3_branch2b'],
+        ['body.10.conv.2.interstellar3c_xception_3x3_branch2c_s',   'body.10.conv.2.interstellar3c_xception_3x3_branch2c_s_bn'],
+        ['body.10.conv.2.interstellar3c_xception_3x3_branch2c',     'body.10.conv.2.bn3c_xception_3x3_branch2c'],
+        ['body.11.conv.0.interstellar3d_xception_3x3_branch2a_s',   'body.11.conv.0.interstellar3d_xception_3x3_branch2a_s_bn'],
+        ['body.11.conv.0.interstellar3d_xception_3x3_branch2a',     'body.11.conv.0.bn3d_xception_3x3_branch2a', 'body.11.conv.0.relu3d_xception_3x3_branch2a'],
+        ['body.11.conv.1.interstellar3d_xception_3x3_branch2b_s',   'body.11.conv.1.interstellar3d_xception_3x3_branch2b_s_bn'],
+        ['body.11.conv.1.interstellar3d_xception_3x3_branch2b',     'body.11.conv.1.bn3d_xception_3x3_branch2b', 'body.11.conv.1.relu3d_xception_3x3_branch2b'],
+        ['body.11.conv.2.interstellar3d_xception_3x3_branch2c_s',   'body.11.conv.2.interstellar3d_xception_3x3_branch2c_s_bn'],
+        ['body.11.conv.2.interstellar3d_xception_3x3_branch2c',     'body.11.conv.2.bn3d_xception_3x3_branch2c'],
+        ['body.12.conv.0.interstellar3e_xception_3x3_branch2a_s',   'body.12.conv.0.interstellar3e_xception_3x3_branch2a_s_bn'],
+        ['body.12.conv.0.interstellar3e_xception_3x3_branch2a',     'body.12.conv.0.bn3e_xception_3x3_branch2a', 'body.12.conv.0.relu3e_xception_3x3_branch2a'],
+        ['body.12.conv.1.interstellar3e_xception_3x3_branch2b_s',   'body.12.conv.1.interstellar3e_xception_3x3_branch2b_s_bn'],
+        ['body.12.conv.1.interstellar3e_xception_3x3_branch2b',     'body.12.conv.1.bn3e_xception_3x3_branch2b', 'body.12.conv.1.relu3e_xception_3x3_branch2b'],
+        ['body.12.conv.2.interstellar3e_xception_3x3_branch2c_s',   'body.12.conv.2.interstellar3e_xception_3x3_branch2c_s_bn'],
+        ['body.12.conv.2.interstellar3e_xception_3x3_branch2c',     'body.12.conv.2.bn3e_xception_3x3_branch2c'],
+        ['body.13.conv.0.interstellar3f_shufflenet_7x7_branch2a',   'body.13.conv.0.bn3f_shufflenet_7x7_branch2a', 'body.13.conv.0.relu3f_shufflenet_7x7_branch2a'],
+        ['body.13.conv.1.interstellar3f_shufflenet_7x7_branch2b_s', 'body.13.conv.1.interstellar3f_shufflenet_7x7_branch2b_s_bn'],
+        ['body.13.conv.1.interstellar3f_shufflenet_7x7_branch2b',   'body.13.conv.1.bn3f_shufflenet_7x7_branch2b'],
+        ['body.14.conv.0.interstellar3g_shufflenet_7x7_branch2a',   'body.14.conv.0.bn3g_shufflenet_7x7_branch2a', 'body.14.conv.0.relu3g_shufflenet_7x7_branch2a'],
+        ['body.14.conv.1.interstellar3g_shufflenet_7x7_branch2b_s', 'body.14.conv.1.interstellar3g_shufflenet_7x7_branch2b_s_bn'],
+        ['body.14.conv.1.interstellar3g_shufflenet_7x7_branch2b',   'body.14.conv.1.bn3g_shufflenet_7x7_branch2b'],
+        ['body.15.conv.0.interstellar3h_shufflenet_3x3_branch2a',   'body.15.conv.0.bn3h_shufflenet_3x3_branch2a', 'body.15.conv.0.relu3h_shufflenet_3x3_branch2a'],
+        ['body.15.conv.1.interstellar3h_shufflenet_3x3_branch2b_s', 'body.15.conv.1.interstellar3h_shufflenet_3x3_branch2b_s_bn'],
+        ['body.15.conv.1.interstellar3h_shufflenet_3x3_branch2b',   'body.15.conv.1.bn3h_shufflenet_3x3_branch2b'],
+        ['body.16.conv.0.interstellar4a_shufflenet_7x7_branch2a',   'body.16.conv.0.bn4a_shufflenet_7x7_branch2a', 'body.16.conv.0.relu4a_shufflenet_7x7_branch2a'],
+        ['body.16.conv.1.interstellar4a_shufflenet_7x7_branch2b_s', 'body.16.conv.1.interstellar4a_shufflenet_7x7_branch2b_s_bn'],
+        ['body.16.conv.1.interstellar4a_shufflenet_7x7_branch2b',   'body.16.conv.1.bn4a_shufflenet_7x7_branch2b'],
+        ['body.16.proj_conv.interstellar4a_proj_s',                 'body.16.proj_conv.interstellar4a_proj_s_bn'],
+        ['body.16.proj_conv.interstellar4a_proj',                   'body.16.proj_conv.bn4a_proj', 'body.16.proj_conv.relu4a_proj'],
+        ['fpn.output1.0', 'fpn.output1.1', 'fpn.output1.2'],
+        ['fpn.output2.0', 'fpn.output2.1', 'fpn.output2.2'],
+        ['fpn.output3.0', 'fpn.output3.1', 'fpn.output3.2'],
+        ['fpn.merge1.0', 'fpn.merge1.1', 'fpn.merge1.2'],
+        ['fpn.merge2.0', 'fpn.merge2.1', 'fpn.merge2.2'],
+        ['ssh1.conv3X3.0',   'ssh1.conv3X3.1'],
+        ['ssh1.conv5X5_1.0', 'ssh1.conv5X5_1.1', 'ssh1.conv5X5_1.2'],
+        ['ssh1.conv5X5_2.0', 'ssh1.conv5X5_2.1'],
+        ['ssh1.conv7X7_2.0', 'ssh1.conv7X7_2.1', 'ssh1.conv7X7_2.2'],
+        ['ssh1.conv7x7_3.0', 'ssh1.conv7x7_3.1'],
+        ['ssh2.conv3X3.0',   'ssh2.conv3X3.1'],
+        ['ssh2.conv5X5_1.0', 'ssh2.conv5X5_1.1', 'ssh2.conv5X5_1.2'],
+        ['ssh2.conv5X5_2.0', 'ssh2.conv5X5_2.1'],
+        ['ssh2.conv7X7_2.0', 'ssh2.conv7X7_2.1', 'ssh2.conv7X7_2.2'],
+        ['ssh2.conv7x7_3.0', 'ssh2.conv7x7_3.1'],
+        ['ssh3.conv3X3.0',   'ssh3.conv3X3.1'],
+        ['ssh3.conv5X5_1.0', 'ssh3.conv5X5_1.1', 'ssh3.conv5X5_1.2'],
+        ['ssh3.conv5X5_2.0', 'ssh3.conv5X5_2.1'],
+        ['ssh3.conv7X7_2.0', 'ssh3.conv7X7_2.1', 'ssh3.conv7X7_2.2'],
+        ['ssh3.conv7x7_3.0', 'ssh3.conv7x7_3.1'],
+
+
+                                          
                                          ], inplace=True)
     print(net)
 
@@ -228,10 +292,13 @@ if __name__ == '__main__':
     #torch.quantization.convert(net, inplace=True)
 
     #net = net.to(device)
-    inp = torch.randn(1, 3, 540, 960)
-    net = torch.jit.trace(net, inp.to(device))
+    inp = torch.randn(1, 3, 540, 960).to(device)
+    if args.mkl:
+        net = mkldnn.to_mkldnn(net)
+        inp = inp.to_mkldnn()
+    net = torch.jit.trace(net, inp)
     #torchnet = optimize_for_mobile(net)
 
-    net.save('retina.pt')
+    net.save('retina-detnas_fuse-mkl.pt')
     test(net, device, args)
 
